efb0d7f0aee1b4b4b678ca0096021af6d9d8d283
mokka86coffee <43421744+mokka86coffee@users.noreply.github.com>
2019-12-02 09:46:17 +0300

    Update saga.js

--- saga.js
+++ saga.js
@@ -1,28 +1,31 @@
 const fetch = (val = 0) => new Promise(r => setTimeout(() => r(val), 200));
+  
+const flow = async function (fn) {
+    const gen = fn();
 
-const flow = async (fn) => {
-    return (async() => {
-        const gen = fn();
-        let end = false, result;
+    let end = false, result;
         
-        while (!end) {
+    while (!end) {
+        if (result && 'then' in result) {
+          result = await result;
+          const {value, done} = gen.next(result);
+          result = value;
+          end = done;
+        } else {
             const {value, done} = gen.next(result);
-            result = await value;
+            result = value;
             end = done;
         }
+    }
 
-        return result;
-    })()
+    return result;
 };
 
-let result;
-
-flow(function*(){
+const fromFlow = flow(function*(){
     const a = yield fetch(1);
-    const b = yield fetch(20);
-    result = b + a;
-    const c = yield fetch(100);
-    result +=  c;
+    const b = yield fetch(2);
+    const c = yield fetch(3);
+    const d = yield fetch('d');
+    const e = yield fetch('e');
+    console.log(a + b + c + d + e); // 6de
 });
-
-setTimeout(() => console.log(result), 1000);
